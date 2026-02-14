7db8f8f8a26d1ad62fe5634221496f5603a54a35
cnshwetasharma <devshwetasharma07@gmail.com>
2026-02-14 22:11:29 +0530

    added file code8.cpp

---
+++ Partice_Qs/23.OOPs/code8.cpp
@@ -0,0 +1,14 @@
+// hierarchial inheritance one class derived into 3- more than 4 class 
+
+// hybrid inheritance - multiply class linked ...
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+    
+    
+    return 0;
+}
