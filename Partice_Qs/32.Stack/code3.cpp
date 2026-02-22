ca0093324d5aa02a89d5307c99025a15c09eee27
cnshwetasharma <devshwetasharma07@gmail.com>
2026-02-23 01:20:19 +0530

    added file code3.cpp

---
+++ Partice_Qs/32.Stack/code3.cpp
@@ -0,0 +1,19 @@
+// STL using in stack ...
+
+#include <iostream>
+using namespace std;
+#include<stack>
+
+int main() {
+    stack<int> s;
+
+    s.push(3);
+    s.push(2);
+    s.push(1);
+     
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+    return 0;
+}
