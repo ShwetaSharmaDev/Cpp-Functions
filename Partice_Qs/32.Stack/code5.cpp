2c8cdf1cda6aab79a47349bf5fd7191552fad445
cnshwetasharma <devshwetasharma07@gmail.com>
2026-02-23 01:20:40 +0530

    added file code5.cpp

---
+++ Partice_Qs/32.Stack/code5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+#include<string>
+#include<stack>
+
+string reverString(string str){
+    string ans;
+    stack<char> s;
+
+    for(int i=0; i < str.size(); i++){
+        s.push(str[i]);
+    }
+
+    while(!s.empty()){
+        char top = s.top();
+        ans += top;
+        s.pop();
+    }
+    return ans;
+}
+
+int main() {
+    string str = "abcd";
+    cout<<reverString(str)<<endl;
+    return 0;
+}
