d74b7f5d5641169cd27814d3a1bde07b12ed4f6d
cnshwetasharma <devshwetasharma07@gmail.com>
2026-02-25 20:34:28 +0530

    added file code2.cpp

---
+++ Partice_Qs/Bitwise_Operators/code2.cpp
@@ -0,0 +1,16 @@
+/* find permutations  
+the number will be n! -> factorial 
+*/
+
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+
+    string str = "abc";
+    string ans = "";
+    
+    return 0;
+}
