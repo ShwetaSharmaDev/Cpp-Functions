c1013664f3c281c777b626d0e3bb9844f73d7391
cnshwetasharma <devshwetasharma07@gmail.com>
2026-02-25 20:34:15 +0530

    added file code.cpp

---
+++ Partice_Qs/Bitwise_Operators/code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    // cout<<(3&5);
+    // cout<<(3|5);
+    // cout<<(3^5);
+    // cout<<(~6);
+    // cout<<(7<<2);
+    // cout<<(7>>2);
+    
+    return 0;
+}
