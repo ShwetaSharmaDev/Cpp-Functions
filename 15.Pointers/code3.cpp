e6cdc427e647656cade02633c69ae79f33c294a4
cnshwetasharma <devshwetasharma07@gmail.com>
2026-03-08 19:59:17 +0530

    added file code3.cpp

--- 15.Pointers/code3.cpp
+++ 15.Pointers/code3.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 int main() {
 
-    int a = 32;
+    int a = 32; 
     int *ptr = &a;
 
-    char ch = 'A';
+    char ch = 'A'; //ascii value 65 + 32 = 97
     char &cho = ch;
 
-    cho += a;
-    *ptr += ch;
+     
+    cho = cho + a; //ch updated to 65 + 32 = 97 A
+    cout<< ch << endl;
+    *ptr += ch; //a updated to 32 + 97 = 129
 
     cout << a << "," << ch << endl;
     
