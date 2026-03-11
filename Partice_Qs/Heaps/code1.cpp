3686e50e19e0f44b51c110a9752127ba9918388b
cnshwetasharma <devshwetasharma07@gmail.com>
2026-03-11 19:49:14 +0530

    Added file code1.cpp

---
+++ Partice_Qs/Heaps/code1.cpp
@@ -0,0 +1,9 @@
+// STL pq
+// heap -> visualize [CBT]
+// code vector/array
+
+/* heap is a complete BT (CBT)
+- CBT is a BT where all levels are filled expect maybe the last one which is filled from left to right...
+
+- parent >= children // max heap 
+ */
