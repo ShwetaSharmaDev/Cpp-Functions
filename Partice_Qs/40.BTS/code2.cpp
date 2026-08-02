4db9c15e093de327e98e9a3499d93f6e9d3788b9
cnshwetasharma <devshwetasharma07@gmail.com>
2026-08-03 01:01:01 +0530

    added file code2.cpp

---
+++ Partice_Qs/40.BTS/code2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// 3 three recursive - preorder inorder postorder 
+
+// 1 iteration - leavel order 
+
+
+
+int main() {
+
+    vector<int> nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+    
+    return 0;
+}
